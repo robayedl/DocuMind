1dfd50c62fd832cb6f2cc86103c2b9b008ed2fea
Robayed Ashraf <robayedashraf@gmail.com>
2026-05-16 21:23:00 +1000

    Convert jest.config.ts to jest.config.js to avoid ts-node dependency in CI

--- web/jest.config.js
+++ web/jest.config.js
@@ -1,9 +1,8 @@
-import type { Config } from "jest";
-import nextJest from "next/jest.js";
+const nextJest = require("next/jest.js");
 
 const createJestConfig = nextJest({ dir: "./" });
 
-const config: Config = {
+const config = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
@@ -12,4 +11,4 @@ const config: Config = {
   modulePathIgnorePatterns: ["<rootDir>/.next/"],
 };
 
-export default createJestConfig(config);
+module.exports = createJestConfig(config);
